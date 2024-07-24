f1c56f359d8582637d5d831a8527c9faf807cf53
SurajNaraleBHGod <rajnarale415@gmail.com>
2024-07-24 23:47:53 +0530

    So There Is I Add extra end point as "/go" in working branch

--- src/main/java/com/docker/DockerExampleApplication.java
+++ src/main/java/com/docker/DockerExampleApplication.java
@@ -2,12 +2,18 @@ package com.docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DockerExampleApplication {
+public class DockerExampleApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerExampleApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder createSpringApplicationBuilder() {
+		return createSpringApplicationBuilder().sources(DockerExampleApplication.class);
+	}
 }
